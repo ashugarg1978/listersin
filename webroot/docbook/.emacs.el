409209d16e9dea0be2decd6b5b1a9d6739c707ee
boo boo <nabe@domU-12-31-39-03-3A-44.compute-1.internal>
2010-03-24 22:56:21 +0900

    delete files

--- webroot/docbook/.emacs.el
+++
@@ -1,6 +0,0 @@
-(add-hook
-  'nxml-mode-hook
-  (lambda ()
-    (setq rng-schema-locating-files-default
-          (append '("/var/www/dev.xboo.st/webroot/docbook/locatingrules.xml")
-                  rng-schema-locating-files-default ))))
--- webroot/docbook/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mydir=$(readlink -f $(dirname $0))
-$mydir/install.sh \
-  --uninstall \
-  --catalogManager=/home/nabe/.resolver/CatalogManager.properties \
-  --dotEmacs= \
-  $@
