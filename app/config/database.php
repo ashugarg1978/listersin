edb5e834fba5f611732557800818a3ef95982b88
boo boo <nabe@domU-12-31-39-03-3A-44.compute-1.internal>
2010-03-25 15:51:00 +0900

    push

--- app/config/database.php
+++ app/config/database.php
@@ -78,7 +78,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => '127.0.0.1',
 		'login' => 'root',
-		'password' => 'kjkp98u',
+		'password' => '',
 		'database' => 'ebay',
 		'prefix' => '',
 	);
