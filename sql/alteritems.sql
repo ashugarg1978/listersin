e910531830723ea52e045901a475468863437ac9
From Netbook <x@x.x>
2010-07-04 13:02:34 +0900

    alter items

--- sql/alteritems.sql
+++ sql/alteritems.sql
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 alter table items add SellingStatus_ListingStatus varchar(20);
-=======
 alter table items add Errors_LongMessage text;
->>>>>>> 76c0fe7f4db1c3b6190f8a07ef04556ea555c816
