c02f5e3a2f69c612188caa577aef0f336dbcebcd
tamazoo <y@y.y>
2010-09-12 20:41:57 +0900

    java and struts2 and maven ...

---
+++ struts2/src/main/java/ebaytool/Ebay.java
@@ -0,0 +1,14 @@
+package ebaytool;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Ebay extends ActionSupport {
+	
+    public String execute() throws Exception {
+		
+		/* do something */
+		
+		return SUCCESS;
+    }
+	
+}
