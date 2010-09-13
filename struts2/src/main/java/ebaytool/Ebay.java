b8e48c1db832791dfa67a2a5aa239fcc1ac19e11
tamazoo <y@y.y>
2010-09-13 23:36:06 +0900

    java -> mongodb connect

--- struts2/src/main/java/ebaytool/Ebay.java
+++ struts2/src/main/java/ebaytool/Ebay.java
@@ -2,14 +2,30 @@ package ebaytool;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import com.mongodb.Mongo;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.DBCursor;
+
 public class Ebay extends ActionSupport {
 	
 	private String msg;
 	
     public String execute() throws Exception {
 		
-		/* do something */
-		setMsg("hogehogefoobar");
+		Mongo m = new Mongo();
+		
+		DB db = m.getDB("ebay");
+		
+		DBCollection coll = db.getCollection("items");
+		
+		DBObject obj = coll.findOne();
+		
+		
+		String title = obj.get("Title").toString();
+		setMsg(title);
 		
 		return SUCCESS;
     }
