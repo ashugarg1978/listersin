5255f47ea34ea93d7a2ddbf8c722562bf5bd5ccc
apac <z@z.z>
2010-10-01 20:15:27 +0900

    api call

--- struts2/src/main/java/UserAction.java
+++ struts2/src/main/java/UserAction.java
@@ -223,19 +223,28 @@ public class UserAction extends ActionSupport {
 		apiContext.setEpsServerUrl("https://api.sandbox.ebay.com/ws/api.dll");
 		apiContext.setSignInUrl("https://signin.sandbox.ebay.com/ws/eBayISAPI.dll?SignIn");
 		
-		ApiCredential apiCred = new ApiCredential();
-		apiContext.setApiCredential(apiCred);
 		
 		ApiAccount ac = new ApiAccount();
-		apiCred.setApiAccount(ac);
-		
 		ac.setDeveloper("e60361cd-e306-496f-ad7d-ba7b688e2207");
 		ac.setApplication("Yoshihir-1b29-4aad-b39f-1be3a37e06a7");
 		ac.setCertificate("8118c1eb-e879-47f3-a172-2b08ca680770");
 		
-		apiCred.seteBayToken("");
+		ApiCredential apiCred = new ApiCredential();
+		apiCred.seteBayToken("AgAAAA**AQAAAA**aAAAAA**BSF/TA**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6wFk4CoD5mFow6dj6x9nY+seQ**Q0UBAA**AAMAAA**JJDsfE+wQFK+EWdZ26syt/D3f1YfYL236IUFs4qTucxrgq0cSr5Pk0YD35LuwFQHLwwJ6Mbz3l+qsQZkF05V/WbkYgbvLu8HG6AtdtL6VdIo941fiW9+XFOvdMgdPI+zFnSp7HNec9+f0plQ7tutQUuu2/t5GmWEEFB3hP9jDVDZ5nD/uNY+332WybjTSwkUUwTQW74fcnEZJB8Afr+gYk9kt71yD5iZMMc6Pbj38FcrYPDLIrhIh5yC7S/Z3EGkUxqPqJuC0SO6iisuG4wG28zJ5vJqQ4lfa+M9VIlOhBADdo2KFaF37wVR0sakHBmmXRDRNfJ5M4DxHX0mW1Y9gHXrjO9IRuG77kjeCA+yvauYJU/QMuidcftlP/QwXZEd4oI1nj/fUyPZbtNMbichZn/5AmEfpJfD3tRkfEyW9QL0Xobw4i5QOi8MVSE2jh+3OjK4J0iKK4naQ9Xiv43uOjPTVbR6Ii3hgGBC+nOXVqgh2DmQpX4vznTl92f5WEyFuNZpLuiyNs3T/QbQ/7ps5POBAnfCwDQMSVBdvRPPOxcXwb8D8uPk2H71VarDxPFYzpew/KVBjpB1nhwXvXWT5uKyq86cYQSanEK7QxUDGu5PcTT2eYB/cZ4lDoviF135Z91ULc4WiVzUpeoHQ7et+eED7bw4VD0HeFfTKwqoQ0VZvPfs3IVhLGswtrlrWmklmMUBuy2d4GF3UInZOUvkkcKw7Sk/nmfLhueixz4iQkWDSi+wxeOumVk86mNoc93H");
+		
+		apiCred.setApiAccount(ac);
+		apiContext.setApiCredential(apiCred);
+		
+		//GetSellerListRequestType req = new GetSellerListRequestType();
 		
 		GetSellerListCall api = new GetSellerListCall(apiContext);
+		
+		api.setUserID("testuser_hal");
+		
+		TimeFilter tf;
+		tf = GuiUtil.getTimeFilterFromFields("2010-06-01", "2010-09-01");
+		api.setStartTimeFilter(tf);
+		
         ItemType[] retItems = api.getEntireSellerList();
 		
 		return SUCCESS;
