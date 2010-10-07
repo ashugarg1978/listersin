8f3d3c95769f4814708f9398a15f875470e9583a
apac <z@z.z>
2010-10-07 18:58:45 +0900

    getSellerList with Java\!

--- struts2/src/main/java/Daemon.java
+++ struts2/src/main/java/Daemon.java
@@ -10,10 +10,14 @@ import com.mongodb.util.*;
 
 import com.thoughtworks.xstream.XStream;
 
+import java.util.LinkedHashMap;
+
 import java.io.*;
 import java.net.URL;
 
 import net.sf.json.JSON;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import net.sf.json.xml.XMLSerializer;
 //import org.apache.commons.io.IOUtils;
 
@@ -26,48 +30,107 @@ import javax.xml.bind.annotation.*;
 
 public class Daemon extends Thread {
 	
-    public static void main(String[] args)
-    {
-		System.out.println("main()");
+    public static void main(String[] args) {
+		
+		System.out.println("main() method");
 		
 		Daemon t = new Daemon();
-		t.setDaemon(true);
+		//t.setDaemon(true);
 		t.start();
-		
 		
 		try {
 			
-			getSellerList();
-			
 			for (int i=0; i<10; i++) {
-				System.out.println("main() method.["+i+"]");
-				//Thread.sleep(600);
+				//System.out.println("main() method.["+i+"]");
 			}
 			
+			//Thread.sleep(10000);
+			
 		} catch (Exception e) {
-			
 			System.out.println(e.toString());
-			
 		}
     }
 	
 	public void run() {
+		
+		System.out.println("run() method");
+		
 		try {
+			
+			System.out.println("run() calling getSellerList()");
+			getSellerList();
+			System.out.println("run() end of getSellerList()");
+			
 			for (int i=0; i<10; i++) {
-				System.out.println("run() method.["+i+"]");
 				//Thread.sleep(1000);
 			}
-		} catch (Exception e) {
 			
+		} catch (Exception e) {
+			System.out.println(e.toString());
 		}
 	}
 	
-	public static String getSellerList() throws Exception {
+	public String getSellerList() throws Exception {
+		
+		String token = "AgAAAA**AQAAAA**aAAAAA**KHmBTA**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6wFk4CoD5mKpw2dj6x9nY+seQ**Q0UBAA**AAMAAA**vIfXjO5I7JEMxVTJem2CIu9tUmKl1ybRTAGc4Bo/RNktrvd+MQ0NMHvUp7qRyWknHZ10fPIGLaSKq0FDQDQVg8hQafeYcmtfPcxvHnESRPSx6IIcad4GPne8vJjvzRgj1quv40pVatq4mId5tRU8D1DwEm930K3JShD92Z+8AXG6qO8TVBf/r4auftBdGNnwStY/01gz0dUXyDhyi3G94yu9Cv8HcyhAvM67yUQKW+45A9WnWuRCrxVgx3xYFUKhTT+8tJb4KtDgH65zfQuk4og6TvqD6qO85FPS+hSpAX7dFYxFPgw5R61VXJBm4LD4seJA1/E+2fA1Ge5UUplH0aS8hTs0yZYIeBx2WHs9OhV5HaAY5lj2kNm3h59GbheSsBfjReMk/Yxm3X9rLRalw20utx4Z4MU+JZgMePouNAcceDHsFRylE+e2nnDfddx3peQOpwrbEtIm9fOqBahBs7MAy+IVVY8CcvoEn+Msoevz18jpTj0P+1h/fBvdliedAPOmMuiafYfqtYmIfTSTWIJzAfvcpBsZD3cW+ilo6GfJ4875x2R221qEUwS1AYT1GIK5Ctip/pKAxKT/ugf18PtLd3FJ5jVWziTsFFZ07ZVjihShtsXLsORQBInvMqE1PgniJ3Hpdsqp85eIo1pwhlLBD/2rsCRTodGOFX9t47RMST1WKAjzAqPW0XnqfPvYfuII7kaqL/YT0pV/eyNzdiFjtXklWGDSPNdQfoSC1Uh7mxMXNxx5HHlV98QS/jTB";
+		
+		/* test */
+		/*
+		LinkedHashMap<String,Object> req = new LinkedHashMap<String,Object>();
+		req.put("aaa", "bbb");
+		req.put("ccc", "ddd");
+		XStream xstream = new XStream();
+        xstream.toXML(req, System.out);
+		*/
+		
+		BasicDBObject test = new BasicDBObject();
+		test.put("DetailLevel", "ReturnAll");
+		test.put("WarningLevel", "High");
+		test.put("RequesterCredentials", new BasicDBObject("eBayAuthToken", token));
+		test.put("StartTimeFrom", "2010-06-01 00:00:00");
+		test.put("StartTimeTo",   "2010-08-01 00:00:00");
+		test.put("Pagination", new BasicDBObject("EntriesPerPage", "50"));
+		test.put("Sort", "1");
+		String js = (String) com.mongodb.util.JSON.serialize(test);
 		
 		
+		JSONObject jso = JSONObject.fromObject(js);
+		XMLSerializer xmls = new XMLSerializer();
+		xmls.setObjectName("GetSellerListRequest");
+		xmls.setNamespace(null, "urn:ebay:apis:eBLBaseComponents");
+		xmls.setTypeHintsEnabled(false);
+		String xml = xmls.write(jso);
 		
+		
+		Mongo m = new Mongo();
+		DB db = m.getDB("ebay");
+		DBCollection coll = db.getCollection("items");
+		
+		JSONObject json = (JSONObject) callapi("GetSellerList", xml);
+		String userid = ((JSONObject) json.get("Seller")).get("UserID").toString();
+		
+		JSONArray jsonarr = json.getJSONObject("ItemArray").getJSONArray("Item");
+		for (Object item : jsonarr) {
+			
+			/* convert JSON to DBObject */
+			DBObject dbobject = (DBObject) com.mongodb.util.JSON.parse(item.toString());
+			dbobject.put("UserID", userid);
+			dbobject.put("deleted", 0);
+			
+			/* insert into mongodb */
+			coll.insert(dbobject);
+			
+			System.out.println(dbobject.get("Title"));
+		}
+		
+		return "ok";
+	}
+	
+	
+	private net.sf.json.JSON callapi(String callname, String xmldata) throws Exception {
 		
         URL url = new URL("https://api.sandbox.ebay.com/ws/api.dll");
+		
         HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
 		
         conn.setRequestMethod("POST");
@@ -76,51 +139,37 @@ public class Daemon extends Thread {
 		
         conn.setRequestProperty("Content-Type", "text/xml");
         conn.setRequestProperty("X-EBAY-API-COMPATIBILITY-LEVEL", "677");
-        conn.setRequestProperty("X-EBAY-API-CALL-NAME", "GetSellerList");
+        conn.setRequestProperty("X-EBAY-API-CALL-NAME", callname);
         conn.setRequestProperty("X-EBAY-API-SITEID", "0");
-        conn.setRequestProperty("X-EBAY-API-DEV-NAME", "e60361cd-e306-496f-ad7d-ba7b688e2207");
-        conn.setRequestProperty("X-EBAY-API-APP-NAME", "Yoshihir-1b29-4aad-b39f-1be3a37e06a7");
+        conn.setRequestProperty("X-EBAY-API-DEV-NAME",  "e60361cd-e306-496f-ad7d-ba7b688e2207");
+        conn.setRequestProperty("X-EBAY-API-APP-NAME",  "Yoshihir-1b29-4aad-b39f-1be3a37e06a7");
         conn.setRequestProperty("X-EBAY-API-CERT-NAME", "8118c1eb-e879-47f3-a172-2b08ca680770");
 		
-		String xmldata;
-		xmldata = "<?xml version=\"1.0\" encoding=\"utf-8\" ?><GetSellerListRequest xmlns=\"urn:ebay:apis:eBLBaseComponents\"><WarningLevel>High</WarningLevel><RequesterCredentials><eBayAuthToken>AgAAAA**AQAAAA**aAAAAA**KHmBTA**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6wFk4CoD5mKpw2dj6x9nY+seQ**Q0UBAA**AAMAAA**vIfXjO5I7JEMxVTJem2CIu9tUmKl1ybRTAGc4Bo/RNktrvd+MQ0NMHvUp7qRyWknHZ10fPIGLaSKq0FDQDQVg8hQafeYcmtfPcxvHnESRPSx6IIcad4GPne8vJjvzRgj1quv40pVatq4mId5tRU8D1DwEm930K3JShD92Z+8AXG6qO8TVBf/r4auftBdGNnwStY/01gz0dUXyDhyi3G94yu9Cv8HcyhAvM67yUQKW+45A9WnWuRCrxVgx3xYFUKhTT+8tJb4KtDgH65zfQuk4og6TvqD6qO85FPS+hSpAX7dFYxFPgw5R61VXJBm4LD4seJA1/E+2fA1Ge5UUplH0aS8hTs0yZYIeBx2WHs9OhV5HaAY5lj2kNm3h59GbheSsBfjReMk/Yxm3X9rLRalw20utx4Z4MU+JZgMePouNAcceDHsFRylE+e2nnDfddx3peQOpwrbEtIm9fOqBahBs7MAy+IVVY8CcvoEn+Msoevz18jpTj0P+1h/fBvdliedAPOmMuiafYfqtYmIfTSTWIJzAfvcpBsZD3cW+ilo6GfJ4875x2R221qEUwS1AYT1GIK5Ctip/pKAxKT/ugf18PtLd3FJ5jVWziTsFFZ07ZVjihShtsXLsORQBInvMqE1PgniJ3Hpdsqp85eIo1pwhlLBD/2rsCRTodGOFX9t47RMST1WKAjzAqPW0XnqfPvYfuII7kaqL/YT0pV/eyNzdiFjtXklWGDSPNdQfoSC1Uh7mxMXNxx5HHlV98QS/jTB</eBayAuthToken></RequesterCredentials><DetailLevel>ReturnAll</DetailLevel><StartTimeFrom>2010-06-01 00:00:00</StartTimeFrom><StartTimeTo>2010-08-30 00:00:00</StartTimeTo><Pagination><EntriesPerPage>50</EntriesPerPage><PageNumber>1</PageNumber></Pagination><Sort>1</Sort></GetSellerListRequest>";
 		
 		OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
 		osw.write(xmldata);
 		osw.flush();
 		osw.close();
 		
-        //PrintWriter output = new PrintWriter(new OutputStreamWriter(conn.getOutputStream()));
-        //String fileContent = convertFileContent2String(XmlFileName);
 		
-        //output.println(fileContent);
-        //output.close();
         conn.connect();
 		
+		
+		/* handle http response */
         InputStreamReader isr = new InputStreamReader(conn.getInputStream());
 		BufferedReader br = new BufferedReader(isr);
 		String line;
-		String allline;
-		allline = "";
+		String response = "";
 		while ((line = br.readLine()) != null) {
-			allline = allline + line;
+			response = response + line;
 		}
 		br.close();
 		
 		
-		/* convert Strint to JSON */
 		XMLSerializer xmlSerializer = new XMLSerializer(); 
-		net.sf.json.JSON json = xmlSerializer.read(allline);
+		net.sf.json.JSON json = xmlSerializer.read(response);
 		
-		/* convert JSON to DBObject */
-		DBObject dbobject = (DBObject) com.mongodb.util.JSON.parse(json.toString(2));
-		
-		/* insert into mongodb */
-		Mongo m = new Mongo();
-		DB db = m.getDB("ebay");
-		DBCollection coll = db.getCollection("test");
-		coll.insert(dbobject);
-		
-		return "ok";
+		return json;
 	}
+	
 }
