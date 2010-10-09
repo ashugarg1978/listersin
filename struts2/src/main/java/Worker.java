ed9b6b8e8194ed4f184ae5b9e0ce491ef19cd309
From Netbook <x@x.x>
2010-10-09 22:51:57 +0900

    thread pool

--- struts2/src/main/java/Worker.java
+++ struts2/src/main/java/Worker.java
@@ -1,29 +1,74 @@
 package ebaytool;
 
+import java.io.*;
+import java.net.URL;
 import java.util.concurrent.*;
+import javax.net.ssl.HttpsURLConnection;
 
-public class Worker implements Runnable {
+public class Worker implements Callable {
 	
-	private int i;
+	private String callname;
+	private String requestxml;
+	private String result;
 	
-	Worker (int j) {
-		i = j;
+	public Worker (String callname, String requestxml) {
+		this.callname = callname;
+		this.requestxml = requestxml;
 	}
 	
-	public void run() {
+	public String call() {
 		
-		for (int k=0; k<20; k++) {
-			
-			System.out.println(i+" : "+k);
-			
-			try {
-				Thread.sleep(1000*(i+1));
-			} catch (Exception e) {
-				
-			}
-			
+		if (callname == "GetSellerList") {
+			result = getSellerList();
 		}
-
+		
+		return result;
+	}
+	
+	public String getSellerList() throws Exception {
+		
+		String token = "AgAAAA**AQAAAA**aAAAAA**KHmBTA**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6wFk4CoD5mKpw2dj6x9nY+seQ**Q0UBAA**AAMAAA**vIfXjO5I7JEMxVTJem2CIu9tUmKl1ybRTAGc4Bo/RNktrvd+MQ0NMHvUp7qRyWknHZ10fPIGLaSKq0FDQDQVg8hQafeYcmtfPcxvHnESRPSx6IIcad4GPne8vJjvzRgj1quv40pVatq4mId5tRU8D1DwEm930K3JShD92Z+8AXG6qO8TVBf/r4auftBdGNnwStY/01gz0dUXyDhyi3G94yu9Cv8HcyhAvM67yUQKW+45A9WnWuRCrxVgx3xYFUKhTT+8tJb4KtDgH65zfQuk4og6TvqD6qO85FPS+hSpAX7dFYxFPgw5R61VXJBm4LD4seJA1/E+2fA1Ge5UUplH0aS8hTs0yZYIeBx2WHs9OhV5HaAY5lj2kNm3h59GbheSsBfjReMk/Yxm3X9rLRalw20utx4Z4MU+JZgMePouNAcceDHsFRylE+e2nnDfddx3peQOpwrbEtIm9fOqBahBs7MAy+IVVY8CcvoEn+Msoevz18jpTj0P+1h/fBvdliedAPOmMuiafYfqtYmIfTSTWIJzAfvcpBsZD3cW+ilo6GfJ4875x2R221qEUwS1AYT1GIK5Ctip/pKAxKT/ugf18PtLd3FJ5jVWziTsFFZ07ZVjihShtsXLsORQBInvMqE1PgniJ3Hpdsqp85eIo1pwhlLBD/2rsCRTodGOFX9t47RMST1WKAjzAqPW0XnqfPvYfuII7kaqL/YT0pV/eyNzdiFjtXklWGDSPNdQfoSC1Uh7mxMXNxx5HHlV98QS/jTB";
+		
+		return "ok";
+	}
+	
+	private String callapi(String callname, String xml) throws Exception {
+		
+        URL url = new URL("https://api.sandbox.ebay.com/ws/api.dll");
+		
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+		
+        conn.setRequestMethod("POST");
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+		
+		/* http request header */
+        conn.setRequestProperty("Content-Type", "text/xml");
+        conn.setRequestProperty("X-EBAY-API-COMPATIBILITY-LEVEL", "677");
+        conn.setRequestProperty("X-EBAY-API-CALL-NAME", callname);
+        conn.setRequestProperty("X-EBAY-API-SITEID", "0");
+        conn.setRequestProperty("X-EBAY-API-DEV-NAME",  "e60361cd-e306-496f-ad7d-ba7b688e2207");
+        conn.setRequestProperty("X-EBAY-API-APP-NAME",  "Yoshihir-1b29-4aad-b39f-1be3a37e06a7");
+        conn.setRequestProperty("X-EBAY-API-CERT-NAME", "8118c1eb-e879-47f3-a172-2b08ca680770");
+		
+		OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+		osw.write(xml);
+		osw.flush();
+		osw.close();
+		
+        conn.connect();
+		
+		/* handle http response */
+        InputStreamReader isr = new InputStreamReader(conn.getInputStream());
+		BufferedReader br = new BufferedReader(isr);
+		String line;
+		String response = "";
+		while ((line = br.readLine()) != null) {
+			response = response + line;
+		}
+		br.close();
+		
+		return response;
 	}
 	
 }
