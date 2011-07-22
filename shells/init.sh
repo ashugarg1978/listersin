4bf004a800073a00221d0fed46e0d9dbdc374a58
devweb <fd3s.boost@gmail.com>
2011-07-22 21:36:53 +0900

    modify init.sh

--- shells/init.sh
+++ shells/init.sh
@@ -2,6 +2,6 @@
 
 /usr/local/mongodb/bin/mongo ebay --eval 'db.dropDatabase();'
 
-echo 'GeteBayDetails' | nc localhost 8181
-echo 'GetCategories' | nc localhost 8181
-echo 'GetCategoryFeatures' | nc localhost 8181
+/var/www/ebaytool.jp/shells/callapi.sh GeteBayDetails
+/var/www/ebaytool.jp/shells/callapi.sh GetCategories
+/var/www/ebaytool.jp/shells/callapi.sh GetCategoryFeatures
