92789dd6ba17a090caffd29866a957db53206436
tamazoo <fd3s.boost@gmail.com>
2012-01-13 23:56:38 +0900

    GetProductSearchResults

--- src/main/java/apicall/GetProductFinder.java
+++ src/main/java/apicall/GetProductFinder.java
@@ -4,7 +4,19 @@ import com.mongodb.*;
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.apache.commons.lang.StringEscapeUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 
 public class GetProductFinder extends ApiCall {
 	
@@ -60,12 +72,41 @@ public class GetProductFinder extends ApiCall {
 			log(classname+" "+idx.toString());
 		}
 		
-		DBCollection coll = db.getCollection(site+".ProductFinder");
-		if (db.collectionExists(site+".ProductFinderCategory2CS.Category")) {
-			coll.drop();
+		File fXmlFile = new File
+			("/var/www/ebaytool.jp/logs/apicall/GetProductFinder/"+site+".decoded.xml");
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(fXmlFile);
+		//doc.getDocumentElement().normalize();
+		
+		NodeList nList = doc.getElementsByTagName("ProductFinder");
+		
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			Node nNode = nList.item(temp);
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+
+				// ref: http://www.mkyong.com/java/how-to-create-xml-file-in-java-dom/
+				// ref: http://www.genedavis.com/library/xml/java_dom_xml_creation.jsp
+				// ref: http://www.java2s.com/Code/JavaAPI/javax.xml.transform/OutputKeysOMITXMLDECLARATION.htm
+				
+				TransformerFactory transformerFactory = TransformerFactory.newInstance();
+				Transformer transformer = transformerFactory.newTransformer();
+				transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+				transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+				
+				DOMSource source = new DOMSource(eElement);
+				
+				StringWriter sw = new StringWriter();
+				StreamResult result = new StreamResult(sw);
+				
+				transformer.transform(source, result);
+				
+				String xmlString = sw.toString();
+				
+				log("====\n\n"+xmlString+"\n\n");
+			}
 		}
-		coll.insert((List<DBObject>) ((BasicDBList) dbo.get("ProductFinders")).get("ProductFinder"));
-		//coll.insert((List<DBObject>) ((BasicDBObject) dbo.get("ProductFinders")).get("ProductFinder"));
 		
 		return "";
 	}
---
+++ src/main/java/apicall/GetProductSearchResults.java
@@ -0,0 +1,41 @@
+package ebaytool.apicall;
+
+import com.mongodb.*;
+import java.io.*;
+import java.util.*;
+
+public class GetProductSearchResults extends ApiCall {
+	
+	public GetProductSearchResults() throws Exception {
+	}
+	
+	public String call() throws Exception {
+		
+		BasicDBObject reqdbo = new BasicDBObject();
+		reqdbo.append("RequesterCredentials", new BasicDBObject("eBayAuthToken", admintoken));
+		reqdbo.append("WarningLevel", "High");
+		reqdbo.append("DetailLevel",  "ReturnAll");
+		reqdbo.append("MessageID",    "US");
+		
+		BasicDBObject productsearch = new BasicDBObject();
+		productsearch.append("QueryKeywords", "blackberry");
+		productsearch.append("CharacteristicSetIDs", new BasicDBObject("ID", "1785"));
+		reqdbo.append("ProductSearch", productsearch);
+		
+		String requestxml = convertDBObject2XML(reqdbo, "GetProductSearchResults");
+		Future<String> future =
+			pool18.submit(new ApiCallTask(0, requestxml, "GetProductSearchResults"));
+		String result = future.get();
+		
+		return result;
+	}
+	
+	public String callback(String responsexml) throws Exception {
+		
+		BasicDBObject resdbo = convertXML2DBObject(responsexml);
+		String site = resdbo.getString("CorrelationID");
+		writelog("GetProductSearchResults/"+site+".xml", responsexml);
+		
+		return "";
+	}
+}
