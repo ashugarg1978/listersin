4697730697a3feb13bf0e0a3199a7b29608da44a
tokyo <fd3s.boost@gmail.com>
2012-02-01 18:33:13 +0900

    auto pagination

--- shells/resetitems.sh
+++ shells/resetitems.sh
@@ -3,10 +3,10 @@
 email='fd3s.boost@gmail.com'
 cmd='/var/www/ebaytool.jp/shells/callapi.sh'
 
-#start=`date +%Y-%m-%d --date '89 day ago'`
-#end=`date +%Y-%m-%d --date '1 day'`
-start=`date +%Y-%m-%d --date '180 day ago'`
-end=`date +%Y-%m-%d --date '90 day ago'`
+start=`date +%Y-%m-%d --date '89 day ago'`
+end=`date +%Y-%m-%d --date '1 day'`
+#start=`date +%Y-%m-%d --date '180 day ago'`
+#end=`date +%Y-%m-%d --date '90 day ago'`
 
 userids=`/usr/local/mongodb/bin/mongo ebay \
 	--eval 'db.users.find({},{"userids":1}).forEach(printjson)' \
@@ -21,4 +21,5 @@ userids=`/usr/local/mongodb/bin/mongo ebay \
 for userid in $userids
 do
 	$cmd GetSellerList $email $userid Start $start $end &
+	$cmd GetSellerList $email $userid End $start $end &
 done
