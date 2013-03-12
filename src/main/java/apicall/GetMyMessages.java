7f1500e37ad6b78b9ba8edc28c25e91a1e84961f
admin <admin@listers.in>
2013-03-12 18:07:15 +0900

    Modify GetMyMessages api call.

--- src/main/java/apicall/GetMyMessages.java
+++ src/main/java/apicall/GetMyMessages.java
@@ -16,15 +16,36 @@ public class GetMyMessages extends ApiCall implements Callable {
 	
 	private String email;
 	private String userid;
-    
+  private String detaillevel;
+  private String starttime;
+  private String endtime;
+  private String[] messageids;
+  
 	public GetMyMessages() throws Exception {
 	}
 	
 	public GetMyMessages(String[] args) throws Exception {
+    
 		this.email  = args[0];
 		this.userid = args[1];
+		this.detaillevel = args[2];
+    
+    if (detaillevel.equals("ReturnMessages")) {
+      
+      messageids = new String[args.length - 3];
+      
+      for (int i=3; i<args.length; i++) {
+        messageids[i-3] = args[i];
+      }
+      
+    } else if (args.length == 5) {
+      
+      this.starttime = args[3];
+      this.endtime = args[4];
+      
+    }
 	}
-	
+  
 	public String call() throws Exception {
     
 		String token = gettoken(email, userid);
@@ -32,11 +53,44 @@ public class GetMyMessages extends ApiCall implements Callable {
     BasicDBObject reqdbo = new BasicDBObject();
     reqdbo.put("RequesterCredentials", new BasicDBObject("eBayAuthToken", token));
     reqdbo.put("WarningLevel", "High");
-    reqdbo.put("DetailLevel", "ReturnMessages");
+    reqdbo.put("DetailLevel", detaillevel);
 		reqdbo.put("MessageID", getnewtokenmap(email) + " " + userid);
+    if (starttime != null) {
+      reqdbo.put("StartTime", starttime);
+    }
+    if (endtime != null) {
+      reqdbo.put("EndTime", endtime);
+    }
     
-    String requestxml = convertDBObject2XML(reqdbo, "GetMyMessages");
-		
+    String requestxml = "";
+    
+    if (detaillevel.equals("ReturnMessages")) {
+      
+      reqdbo.put("MessageIDs", new BasicDBObject("MessageID", messageids));
+      
+      /* expand <e></e> elements */
+      String jss = reqdbo.toString();
+      
+      JSONObject jso = JSONObject.fromObject(jss);
+      
+      JSONObject tmpids = jso.getJSONObject("MessageIDs");
+      tmpids.getJSONArray("MessageID").setExpandElements(true);
+      
+      XMLSerializer xmls = new XMLSerializer();
+      xmls.setObjectName("GetMyMessages");
+      xmls.setNamespace(null, "urn:ebay:apis:eBLBaseComponents");
+      xmls.setTypeHintsEnabled(false);
+      
+      requestxml = xmls.write(jso);
+      
+    } else {
+      
+      requestxml = convertDBObject2XML(reqdbo, "GetMyMessages");
+      
+    }
+    
+		writelog("GetMyMessages/" + userid + "." + detaillevel + ".req.xml", requestxml);
+    
     Future<String> future = pool18.submit(new ApiCallTask(0, requestxml, "GetMyMessages"));
 		future.get();
     
@@ -57,5 +111,5 @@ public class GetMyMessages extends ApiCall implements Callable {
     
 		return "";
 	}
-    
+  
 }
